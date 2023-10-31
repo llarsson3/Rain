f2e015978d588624aa6c5a00a4704007a9e2eec7
Leyla Larsson <llarsson3@outlook.com>
2023-10-31 19:13:35 +0100

    gen rda and rds

---
+++ RainForecast_DataManipulation.R
@@ -0,0 +1,30 @@
+rm(list = ls())
+
+# Data manipulation to get the dataset in shape for the data dashboard
+
+library(haven)
+library(labelled)
+library(foreign)
+library(sjlabelled)
+library(gtsummary)
+library(naniar)
+library(lubridate)
+library(lisa)
+library(ggpubr)
+library(gt)
+library(knitr)
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(ggforce)
+library(ggdist)
+library(gghalves)
+library(magrittr)
+library(RColorBrewer)
+
+rain <- read_excel("BdL_Rain.xlsx")
+
+rain$datetoday <- as.Date("2023-10-31")
+
+saveRDS(rain,file="RainForecast.Rds")
+save(rain,file="RainForecast.Rds.Rda")
